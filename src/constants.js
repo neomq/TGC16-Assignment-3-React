0350631368cb0e223eafb20a949bb8d50b2579b6
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2023-11-17 02:26:51 +0800

    add feature section on landing page

--- src/constants.js
+++ src/constants.js
@@ -16,23 +16,23 @@ const mobileMenu = [
 
 const featuredContent = [
     {
-        label: "Beauty",
+        label: "Beauty Range",
         title: "Youthful Skin",
-        image: "http://res.cloudinary.com/dnxbnmoyc/image/upload/v1699689245/ya7qidgehypakpzn2exp.webp",
+        image: "https://res.cloudinary.com/dnxbnmoyc/image/upload/v1699989340/kndov52cdc40iebnpocc.jpg",
         description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
         button: "Shop Range"
     },
     {
-        label: "Body",
+        label: "Body Range",
         title: "Immunity Boost",
-        image: "http://res.cloudinary.com/dnxbnmoyc/image/upload/v1699689245/ya7qidgehypakpzn2exp.webp",
+        image: "https://res.cloudinary.com/dnxbnmoyc/image/upload/v1700065249/eo_collection1_mtbd7s.webp",
         description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
         button: "Shop Range"
     },
     {
-        label: "Mind",
+        label: "Senses Range",
         title: "Emotional Healing",
-        image: "http://res.cloudinary.com/dnxbnmoyc/image/upload/v1699689245/ya7qidgehypakpzn2exp.webp",
+        image: "https://res.cloudinary.com/dnxbnmoyc/image/upload/v1700065249/eo_collection3_r0heua.webp",
         description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
         button: "Shop Range"
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,30 +13,33 @@ export default function Home({ setBannerHeight }) {
             <div className="bg position-relative" ref={mainBannerRef}>
                     <div className="header-content position-absolute">
                         <div className="cta d-flex flex-column position-absolute top-50 start-50 translate-middle">
-                            <h1 className="cta m-0 title text-center">Essential Oils for Your Mind & Body</h1>
+                            <p className="cta m-0 title text-center">Essential Oils For Your Mind & Body</p>
                             <p className="cta mt-4 short-text text-center text-white">Elevate your everyday with the power of nature's essence.</p>
                             <a className="shop-btn btn mx-auto mt-3 text-uppercase" href="/products" role="button">Shop now</a>
                         </div>
                     </div>
             </div>
             <div className="section">
-                <div className="section-content m-auto">
-                    <div className="section-header mt-5 px-2">
+                <div className="section-content">
+                    <div className="section-header px-2 w-75 m-auto">
                         <p className="section-title mb-1 text-center">Explore Essential Oils</p>
                         <p className="section-subtitle pb-2 text-center">Transform your home into a sanctuary of serenity with our premium essential oils, crafted to enhance both mental clarity and physical vitality.</p>
                     </div>
-                    <div className="section-body row row-cols-1 row-cols-md-3 g-3 mt-4 m-auto">
+                    <div className="section-body row row-cols-1 row-cols-md-3 g-3 mt-4 mb-5 m-auto">
                         {/* card */}
                         {featuredContent.map((item, index) => (
                             <div className="col" key={index}>
                                 <div className="card h-100">
-                                    <div className="card-body">
-                                        <p className="card-label m-0 text-uppercase">{item.label}</p>
-                                        <h5 className="card-title mt-3 mb-0">{item.title}</h5>
+                                    <div className="card-body header" style={{
+                                        backgroundImage: `url(${item.image})`,
+                                    }}>
+                                        <div className="pb-5" >
+                                            <p className="card-label m-0 text-uppercase text-white">{item.label}</p>
+                                            <h5 className="card-title text-white m-0 mt-3">{item.title}</h5>
+                                        </div>
                                     </div>
-                                    <img src={item.image} className="card-img-top rounded-0" alt="..." />
                                     <div className="card-body">
-                                        <p className="card-text mt-0 mb-4 pb-2">{item.description}</p>
+                                        <p className="card-text m-0 pb-4">{item.description}</p>
                                         <button type="button" className="btn card-btn text-uppercase">{item.button}</button>
                                     </div>
                                 </div>
