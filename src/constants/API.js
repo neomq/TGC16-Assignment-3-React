1ce54b1490da4534745f3b66615e0ff1883f8f50
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2023-11-17 17:01:14 +0800

    update API.js

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,5 +1,14 @@
 const API = {
-    API_PROFILE: '/api/users/profile'
+    PROFILE: '/api/users/profile',
+    LOGIN: '/api/users/login/',
+    LOGOUT: '/api/users/logout',
+    REGISTER: '/api/users/register',
+    CART: '/api/cart/',
+    PRODUCTS: '/api/products/',
+    PRODUCT_TYPES: '/api/products/types',
+    PRODUCT_USAGE: '/api/products/usages',
+    PRODUCT_SCENT: '/api/products/scents',
+    PRODUCT_BENEFITS: '/api/products/benefits',
 }
 
 export default API
