acd9ad5f06c692bd8e72142317e6cc7330c4abd9
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2023-12-20 16:38:40 +0800

    clean up

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,27 +2,17 @@ import React, { Fragment } from 'react';
 import { Form } from 'react-bootstrap';
 
 export default function TextInput({
-    type,
-    name,
-    value,
-    onChange,
-    className,
-    placeholder,
-    errorState = false,
-    errorMessage = "",
     onFocus = ()=>{},
     onBlur = ()=>{},
-    onKeyDown = ()=>{}
+    onKeyDown = ()=>{},
+    errorState = false,
+    errorMessage = "",
+    ...props
 }) {
     return (
         <Fragment>
             <Form.Control
-                type={type}
-                name={name}
-                value={value}
-                onChange={onChange}
-                className={className}
-                placeholder={placeholder}
+                {...props}
                 autoComplete="off"
                 onFocus={onFocus}
                 onBlur={onBlur}
